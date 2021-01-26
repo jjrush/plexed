dd72dc0aa93cabdb0db7a07c0b460475556d863f
Jason Rush <jrush@nevada.unr.edu>
2021-01-26 00:30:27 -0800

    adding custom help command, making commands return code blocks for formatting, adding status to server, moving new token to config file outisde repo

--- plexed.py
+++ plexed.py
@@ -1,22 +1,40 @@
 import psutil
 import discord
+import sys
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.', help_command=None)
+
+# get the discord token for running the client bot
+f = open("C:\\discord-bot\\plexed-token.txt", "r")
+discordToken = f.read()
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you watch Plex | .help'))
     print('Bot is ready.')
 
-@client.command(aliases=['ram','load','serverload'])
-async def cpu(ctx):
+@client.command(aliases=['ram','cpu','serverload'])
+async def load(ctx):
     # get the cpu usage
     cpu = psutil.cpu_percent()
     # get the ram as a percentage used
     ram = psutil.virtual_memory().percent
     # build response string
-    response = "Plex Server Load: " + "\n" + "CPU: " + str(cpu) + "%" + "\n" + "RAM: " + str(ram) + "%"
+    response =  "```" + \
+                "Plex Server Load: \n" + \
+                "CPU: " + str(cpu) + "%\n" + \
+                "RAM: " + str(ram) + "%\n" + \
+                "```"
     # reply with response
     await ctx.send(response)
 
-client.run('insert your token here')
+@client.command()
+async def help(ctx):
+    response =  "```" + \
+                ".help : display this message\n" + \
+                ".load : displays the RAM and CPU usage on the server" + \
+                "```"
+    await ctx.send(response)
+
+client.run(discordToken)
